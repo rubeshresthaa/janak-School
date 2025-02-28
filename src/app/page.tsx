27979a6edb0ad2cbdb99ea0760b7dcf47104b77f
Raju <rajutamang3456@gmail.com>
2025-02-28 12:23:27 +0545

    feat: RTK

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import UsersList from "./components/UserList";
 export default function HomePage() {
   return (
     <div>
-      <h1>Welcome to Next.js + RTK Query</h1>
+      <h1>this is homepage</h1>
       <UsersList />
     </div>
   );
