9aefa005703548e113181c7709fa8d81c1c4f2bc
rubeshresthaa <rubesh.shrestha619@gmail.com>
2025-03-03 12:31:13 +0545

    Resolve Conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 import { Suspense } from "react";
 import UsersList from "./components/UserList";
 import { HeroSection } from "@/components/others/HeroSection";
-
 import ContactUs from "./Features/ContactUs";
-
 export default function HomePage() {
   return (
     <Suspense>
       <section>
         <HeroSection />
         <UsersList />
-       
         <ContactUs />
       </section>
     </Suspense>
