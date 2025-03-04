aba171233e6b864fc91c7336a6db42a8743796c1
rubeshresthaa <rubesh.shrestha619@gmail.com>
2025-03-04 11:06:50 +0545

    demo blog page

--- src/app/Features/Blog.tsx
+++ src/app/Features/Blog.tsx
@@ -1,71 +1,92 @@
-import Image from "next/image";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Button } from "@/components/ui/button";
 
 const Blogs = () => {
-  const imageCollection = [
+  const blogCollection = [
     {
-      name: "Butterfly",
-      img: "https://plus.unsplash.com/premium_photo-1698524612460-32d35c637488?q=80&w=2089&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Turning Challenges into Opportunities",
+      subTitle: "A Student's Journey",
+      image:
+        "https://images.unsplash.com/photo-1529390079861-591de354faf5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      description:
+        "Every journey, no matter how challenging, holds the promise of growth and transformation. As students, we are not just learning from textbooks, but also from the ups and downs that shape us into stronger, wiser individuals. This is the power of challenges—they push us to become the best version of ourselves.",
+      author: "Rama Limbu",
+      publishedOn: "28th of February",
     },
     {
-      name: "Butterfly1",
-      img: "https://plus.unsplash.com/premium_photo-1724384503241-86f05f2a1bf4?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Stay Positive, Stay Strong",
+      subTitle: "The Power of Positive Thinking in Student Life",
+      image:
+        "https://plus.unsplash.com/premium_photo-1661544204747-272071b388b8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      description:
+        "Student life is filled with highs and lows—exams, assignments, deadlines, and expectations. At times, it can feel overwhelming, but the way you think plays a crucial role in shaping your experiences. Positive thinking is not just about ignoring problems; it’s about facing them with confidence and a solution-oriented mindset. In this article, we’ll explore how a positive attitude can transform student life and lead to success.",
+      author: "Rama Limbu",
+      publishedOn: "28th of February",
     },
   ];
+
   return (
-    <div>
-      <h1 className="text-center font-bold text-3xl text-red-800 my-5">
-        Blogs
-      </h1>
-      <h1 className="w-full bg-green-600 p-2 lg:p-4 text-center text-white font-bold text-lg lg:text-2xl">
-        Whisper Grace:The Enchanting Dance of the Butterfly.
-      </h1>
-      <div className="flex flex-col gap-2 px-4 sm:px-20">
-        {/* images */}
-        <div className="grid md:grid-cols-2 w-full gap-4 p-5">
-          {imageCollection.map((item, index) => (
-            <div className="w-full h-96" key={index}>
-              <Image
-                key={index}
-                src={item.img}
-                alt="blog's image"
-                height={0}
-                width={0}
-                sizes="100vw"
-                className="object-cover w-full h-full rounded-md"
-                objectFit="cover"
-              />
-            </div>
-          ))}
+    <div className="my-3 mx-3 lg:mx-10">
+      <h1 className="text-center font-bold text-red-700 text-4xl">Blogs</h1>
+      <Carousel>
+        <CarouselContent>
+          {blogCollection.map((blog, index) => {
+            return (
+              <CarouselItem key={index}>
+                <div className="relative space-y-2 my-3">
+                  <h1 className="text-center text-2xl lg:text-3xl font-bold">
+                    {blog.title}
+                  </h1>
+                  <h1 className="text-center text-2xl lg:text-3xl font-bold">
+                    {blog.subTitle}
+                  </h1>
+                  <p className="lg:text-justify px-2 py-2 lg:px-4 lg:py-4">
+                    {blog.description}
+                  </p>
+                  <div className="relative">
+                    <img
+                      src={blog.image}
+                      alt={blog.title}
+                      className="w-full h-[300px] lg:h-[500px] object-cover"
+                    />
+                    {/* Author and Published Info*/}
+                    <div className="absolute bottom-2 left-0 right-0 flex gap-4 px-4 py-2  text-white text-md lg:text-lg rounded-md">
+                      <p>
+                        Written By:
+                        <br /> {blog.author}
+                      </p>
+                      <p>
+                        Published On:
+                        <br /> {blog.publishedOn}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </CarouselItem>
+            );
+          })}
+        </CarouselContent>
+        {/* Carousel Buttons */}
+        <CarouselPrevious className="absolute top-2/3 left-5 transform -translate-y-1/2 z-10 text-black p-4 rounded-full">
+          {/* Previous Button */}
+        </CarouselPrevious>
+        <CarouselNext className="absolute top-2/3 right-5 transform -translate-y-1/2 z-10 text-black p-4 rounded-full">
+          {/* Next Button */}
+        </CarouselNext>
+        <div className="flex justify-center items-center">
+          <Button className="p-4 py-6 w-full md:w-54 bg-green-500 text-xl text-white hover:bg-green-700 cursor-pointer">
+            Read More
+          </Button>
         </div>
-
-        {/* Blog content */}
-        <div className="space-y-4">
-          <p className="text-justify">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
-            deserunt dolores natus dicta, consequatur hic, placeat eaque,
-            doloremque ipsa illum commodi ullam at. Sed modi earum asperiores
-            vero delectus nisi debitis sint. Quis consequatur, doloribus
-            repellendus totam eaque nulla tenetur atque! Eum aut soluta tempore,
-            repudiandae ad vitae sed impedit nihil fugiat incidunt deserunt
-            accusantium doloribus esse dolorem obcaecati, distinctio sit,
-            asperiores deleniti quod quos eaque maxime quis. Provident earum
-            laboriosam dolor eligendi? Consequatur, voluptatibus! Enim pariatur
-            vero, alias perferendis corrupti id quasi laudantium voluptatibus
-            animi? Officiis est laudantium quod eius velit esse, accusantium
-            nesciunt aliquam tempore quibusdam excepturi cumque.
-          </p>
-
-          <p className="italic px-4">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi, ex.
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis,
-            tenetur!
-          </p>
-        </div>
-
-        {/* for arrows */}
-        <div></div>
-      </div>
+      </Carousel>
     </div>
   );
 };
+
 export default Blogs;
