a38a1ff36d1c7d59c82500323dedf09accc82395
Raju <rajutamang3456@gmail.com>
2025-03-07 12:25:24 +0545

    fix: navigation

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,36 +1,3 @@
-// import Image from "next/image";
-// import Link from "next/link";
-// import React from "react";
-
-// const Navbar = () => {
-//   return (
-//     <div className="flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-32 py-4">
-//       <div className="flex items-center gap-2 sm:gap-3 lg:gap-5">
-//         <div className="w-10 h-10 sm:w-14 sm:h-14">
-//           <Image
-//             objectFit="contain"
-//             src="/school_logo.png"
-//             alt="school logo"
-//             width="0"
-//             height="0"
-//             sizes="100vw"
-//             className="object-contain w-full h-full"
-//           />
-//         </div>
-//         <h1 className="text-sm sm:text-lg font-semibold flex sm:flex-row flex-col">
-//           <span>Janak Smriti</span> <span>Secondary School</span>
-//         </h1>
-//       </div>
-//       <div className="flex gap-5">
-
-//         <Link href="#blog" smooth>Blog</Link>
-//         <Link href="#">Contact</Link>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Navbar;
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -88,9 +55,15 @@ const Navbar = () => {
               </SheetTitle>
             </SheetHeader>
             <div className="flex flex-col gap-4 p-3 font-medium text-lg">
-              <Link href="#home" className="border-b-2 border-black pb-2">Home</Link>
-              <Link href="#blog" className="border-b-2 border-black pb-2">Blog</Link>
-              <Link href="#contact" className="border-b-2 border-black pb-2">Contact</Link>
+              <Link href="#home" className="border-b-2 border-black pb-2">
+                Home
+              </Link>
+              <Link href="#blog" className="border-b-2 border-black pb-2">
+                Blog
+              </Link>
+              <Link href="#contact" className="border-b-2 border-black pb-2">
+                Contact
+              </Link>
             </div>
           </SheetContent>
         </Sheet>
